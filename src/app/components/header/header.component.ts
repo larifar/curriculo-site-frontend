4da237d60a04fe58cb1922e96764648730ed047c
Larissa Faria <lari.f4ria@gmail.com>
2025-07-01 14:58:53 -0300

    create logout button

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+@Input() name: string = '';
+@Output() logout = new EventEmitter<void>();
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,15 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Router } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
+import { AuthServiceService } from '../../services/auth-service.service';
 
 @Component({
   selector: 'app-home',
-  imports: [ NgIf],
+  imports: [NgIf, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 profile:any;
+private authService = inject(AuthServiceService);
 private http = inject(HttpClient);
 private router = inject(Router);
 
@@ -21,5 +24,11 @@ ngOnInit() {
       next: data => this.profile = data,
       error: () => this.router.navigate(['/login'])
     });
+}
+
+handleLogout() {
+this.authService.signOut()
+  .then(() => this.router.navigate(['/login']))
+  .catch(console.error);
 }
 }
