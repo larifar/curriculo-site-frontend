f5d329f04c2e6c47a1a0fa39444cc4de7ea6cbd8
Larissa Faria <lari.f4ria@gmail.com>
2025-07-01 15:25:53 -0300

    add angular material and interceptor handle error 401

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
@@ -12,5 +14,10 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideHttpClient(
       withFetch(), withInterceptors([authInterceptor])
-    )]
+    ),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      MatSnackBarModule
+    )
+  ]
 };
